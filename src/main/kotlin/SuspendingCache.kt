733974e7f9ed9fd80b8f32adb3e26b2886c8c94f
marcinmoskala <marcinmoskala@gmail.com>
2023-04-14 16:01:13 +0200

    Improve API

--- src/main/kotlin/SuspendingCache.kt
+++ src/main/kotlin/SuspendingCache.kt
@@ -1,25 +1,24 @@
 package com.marcinmoskala
 
 import com.github.benmanes.caffeine.cache.AsyncCache
-import com.github.benmanes.caffeine.cache.Cache
 import com.github.benmanes.caffeine.cache.Caffeine
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.async
-import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.*
 import kotlinx.coroutines.future.asCompletableFuture
+import kotlinx.coroutines.future.asDeferred
 import kotlinx.coroutines.future.await
-import kotlinx.coroutines.supervisorScope
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executor
 
-class SuspendingCache<P : Any, T>(
-    private val delegate: AsyncCache<P, T>,
-) : Cache<P, T> by delegate.synchronous() {
+class SuspendingCache<K : Any, V>(
+    private val cache: AsyncCache<K, V>,
+) {
 
     suspend fun get(
-        key: P,
-        build: suspend (key: P) -> T
-    ): T = supervisorScope {
+        key: K,
+        build: suspend (key: K) -> V
+    ): V = supervisorScope {
         try {
-            delegate.get(key) { k, _ ->
+            cache.get(key) { k, _ ->
                 async { build(k) }.asCompletableFuture()
             }!!.await()
         } catch (_: CancellationException) {
@@ -28,7 +27,36 @@ class SuspendingCache<P : Any, T>(
         }
     }
 
-    fun asyncCache() = delegate
+    fun asAsyncCache() = cache
+
+    fun contains(key: K): Boolean {
+        val async = cache.getIfPresent(key)
+        return async != null && !async.isCancelled
+    }
+
+    suspend fun getIfPresent(key: K): V? = cache.getIfPresent(key)?.await()
+
+    fun put(key: K, value: V) {
+        cache.put(key, CompletableFuture.completedFuture(value))
+    }
+
+    operator fun set(key: K, value: V) = put(key, value)
+
+    suspend fun asMap(): Map<K, V> {
+        return cache.asMap().mapValues { it.value.await() }
+    }
+
+    fun asDeferredMap(): Map<K, Deferred<V>> {
+        return cache.asMap().mapValues { it.value.asDeferred() }
+    }
+
+    fun invalidate(key: K) {
+        cache.synchronous().invalidate(key)
+    }
+
+    fun invalidateAll() {
+        cache.synchronous().invalidateAll()
+    }
 }
 
 fun <K : Any, V> Caffeine<in K, in V>.buildSuspending(): SuspendingCache<K, V> {
